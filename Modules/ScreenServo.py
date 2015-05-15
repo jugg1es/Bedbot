e87620c337debcfccb233538fbc5e343e9dfd64e
jugg1es <peter.roca@gmail.com>
2015-05-14 23:46:52 -0400

    modified

--- Modules/ScreenServo.py
+++ Modules/ScreenServo.py
@@ -94,7 +94,7 @@ class ScreenServo(QObject):
              angleTracker += angleDir
              self.setAngle(angleTracker)
              time.sleep(self.moveSpeed)
-         self.pi.set_servo_pulsewidth(self.servo, 0)
+         #self.pi.set_servo_pulsewidth(self.servo, 0)
          return angleTracker
          
      
