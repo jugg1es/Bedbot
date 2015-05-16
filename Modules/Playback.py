0b984531ec7c83d5af667dada543c7e52f6182af
jugg1es <peter.roca@gmail.com>
2015-05-15 23:37:51 -0400

    Fixed servo hopefully

--- Modules/Playback.py
+++ Modules/Playback.py
@@ -4,12 +4,20 @@ from PyQt4.QtCore import QObject
 
 from perpetualTimer import perpetualTimer
 from threading import Timer,Thread,Event
-from Modules.Widgets.ClockWidget import *
+from Modules.Widgets.PlaybackWidget import *
 
 class Playback(QObject):
 
     Enabled = True    
     menuOrder = 1
+
+    ListenForPinEvent = True
+
+    audioOnPin = None
+    audioOffPin = None
+
+    audioSwitchOnePin = None
+    audioSwitchTwoPin = None
 
     def __init__(self):
         super(Playback, self).__init__()
@@ -22,9 +30,10 @@ class Playback(QObject):
         self.playback_widget.setVisible(False)
 
     def addMenuWidget(self, parent):
-        self.playback_widget = ClockWidget(parent)       
+        self.playback_widget = PlaybackWidget(parent)       
         self.playback_widget.setGeometry(QtCore.QRect(0, 0, 320, 210))  
         self.playback_widget.setVisible(False)
+        self.playback_widget.initialize()
 
     def getMenuIcon(self):
         return "icons/connection.svg"
@@ -37,6 +46,17 @@ class Playback(QObject):
     def getMenuIconWidth(self):
         return 70
 
+    def setPin(self, pinConfig):
+        self.audioOnPin =  pinConfig["ON_BUTTON"]
+        self.audioOffPin =  pinConfig["OFF_BUTTON"]
+        self.audioSwitchOnePin =  pinConfig["AUDIO_ONE_SWITCH"]
+        self.audioSwitchTwoPin =  pinConfig["AUDIO_TWO_SWITCH"]
+
+    def processPinEvent(self, pinNum):
+        if(self.audioOnPin == pinNum):
+            print("audio on")
+        elif(self.audioOffPin == pinNum):
+            print("audio off")
 
    
 
