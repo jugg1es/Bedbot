66e377c3e21cee6cfdb5d8124a21ec4adc0edbf2
jugg1es <peter.roca@gmail.com>
2015-05-28 22:05:13 -0400

    removed web stuff and added better radio preset

--- Scripts/screenPowerControl.sh
+++ Scripts/screenPowerControl.sh
@@ -2,16 +2,16 @@
 c=$2
 
 turnOn() {
-	sudo sh -c "echo '1' > /sys/class/gpio/gpio252/value"
+	sudo sh -c "echo '1' > /sys/class/gpio/gpio508/value"
 }
 
 turnOff() {
-	sudo sh -c "echo '0' > /sys/class/gpio/gpio252/value"
+	sudo sh -c "echo '0' > /sys/class/gpio/gpio508/value"
 }
 
 init() {
-	sudo sh -c "echo 252 > /sys/class/gpio/export"
-	sudo sh -c "echo 'out' > /sys/class/gpio/gpio252/direction"
+	sudo sh -c "echo 508 > /sys/class/gpio/export"
+	sudo sh -c "echo 'out' > /sys/class/gpio/gpio508/direction"
 }
 
 case $1 in
